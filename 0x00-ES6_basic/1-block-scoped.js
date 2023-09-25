8bac22530e8322db88b8d94736f97c79927c7767
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-09-26 01:22:08 +0300

    see diff

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true;
