73ed03c1490dcb2e43bdf13e87f01600a6025f6d
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-09-26 17:43:32 +0300

    Use `const` for non-mutating symbol `item`

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (let item of array) {
+  for (const item of array) {
     newArray.push(appendString + item);
   }
 
