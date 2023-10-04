288231282eecd924620f5b0ce46d30498cdeac09
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-04 06:00:06 +0300

    see diff

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,18 @@
-// `updateStudentGradeByCity`- return array of students for a specific city with their new grade
+/**
+ * `updateStudentGradeByCity`- return array of students for a specific city with their new grade
+ * `students` is filtered to retain an array of students who's location matches `city` (String)
+ * Each object in the filtered array is updated with the `grade` property of
+ * the object with a matching `id` in the array `newGrades`
+ * If no grade is available for a student, the grade property should be 'N/A'
+ */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const filtered = students.filter((student) => student.location === city);
+  const studentsByCity = students.filter((student) => student.location === city);
+  const studentsWithGrades = studentsByCity.map((student) => {
+    const grade = newGrades.filter((newGrade) => newGrade.id === student.id);
+    if (grade[0]) {
+      return { ...student, grade: grade[0].grade };
+    }
+    return { ...student, grade: 'N/A' };
+  });
+  return studentsWithGrades;
 }
