a97901629ba0ff5ca974bc2c3aad5071c815e514
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-05 22:05:15 +0300

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats"""
+    return a + b
