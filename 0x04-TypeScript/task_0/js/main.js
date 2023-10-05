50de89262363846446a3a184635b9dac1517b42d
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-05 16:53:24 +0300

    Revert to task specifications

--- 0x04-TypeScript/task_0/js/main.js
+++
@@ -1,4 +0,0 @@
-var student1 = { firstName: 'John', lastName: 'Doe', age: 18, location: 'New York' };
-var student2 = { firstName: 'Jane', lastName: 'Doe', age: 19, location: 'New York' };
-var studentsList = [student1, student2];
-console.table(studentsList);
