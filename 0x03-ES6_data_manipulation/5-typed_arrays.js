f9bcdd2aed0f360bcf8ce92e4600be17248c40f6
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-06 21:16:43 +0300

    Task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+/**
+ * `createInt8TypedArray` - return a new `ArrayBuffer` with an `Int8 `value at a specific position
+ * Args: `length` (Number), `position` (Number) and `value` (Number)
+ * Throws: `Position outside range` if adding the value is not possible.
+ */
+
+export default function createInt8TypedArray(length, position, value) {
+  if (position > length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
+}
