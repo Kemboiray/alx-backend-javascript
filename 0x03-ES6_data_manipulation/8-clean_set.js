b9bfc1fb305d0339d61b4a02e5f7379a2a87f159
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-07 05:36:38 +0300

    see diff

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,6 +3,6 @@
  * The return value is a '-' separated list of matches
  */
 export default function cleanSet(set, startString) {
-  const matches = Array.from(set).filter(item => item.startsWith(startString));
-  return matches.map(item => item.slice(startString.length)).join('-');
+  const matches = Array.from(set).filter((item) => item.startsWith(startString));
+  return matches.map((item) => item.slice(startString.length)).join('-');
 }
