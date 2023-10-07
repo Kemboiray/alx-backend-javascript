a08fb23c75b8c7cb72603a6c862708d63606e77a
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-07 05:21:20 +0300

    Task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+/**
+ * `cleanSet` returns a string of all set values that start with a specified substring.
+ * The return value is a '-' separated list of matches
+ */
+export default function cleanSet(set, startString) {
+  const matches = Array.from(set).filter(item => item.startsWith(startString));
+  return matches.map(item => item.slice(startString.length)).join('-');
+}
