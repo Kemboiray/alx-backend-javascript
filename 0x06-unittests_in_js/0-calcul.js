d8ed1ea6b02627a7532fa7940cdc4798a1409324
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-24 15:24:07 +0300

    Update

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+// This file defines `calculateNumber`, a function that rounds two numbers and
+// returns the sum of those rounded numbers.
+
+function calculateNumber(a, b) {
+  return Math.round(a) + Math.round(b);
+}
