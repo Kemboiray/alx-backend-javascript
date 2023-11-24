2e18c84518f278bf5e7ac9c5cbe100bf7e4709f6
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-24 15:41:20 +0300

    Task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -4,3 +4,5 @@
 function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
 }
+
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -28,8 +28,8 @@ describe('calculateNumber', () => {
   it('should return -5 when adding -1.2 and -3.7', () => {
     assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
   });
-  it('should return -6 when adding -1.5 and -3.7', () => {
-    assert.strictEqual(calculateNumber(-1.5, -3.7), -6);
+  it('should return -5 when adding -1.5 and -3.7', () => {
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
   });
   it('should return -4 when adding -0.1 and -0.3', () => {
     assert.strictEqual(calculateNumber(-0.1, -0.3), -0);
