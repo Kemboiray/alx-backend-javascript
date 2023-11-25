d1e1831c12ee70d61d5e984d9d6a831e99bba4e7
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-11-25 02:32:43 +0000

    Cirrect tests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,6 +4,7 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
+  // SUM
   it('should return 6 when adding 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
   });
@@ -16,16 +17,21 @@ describe('calculateNumber', () => {
   it('should return -5 when adding -1 and -3.7', () => {
     assert.strictEqual(calculateNumber('SUM', -1, -3.7), -5);
   });
-  it('should return 6 when subtracting 1.5 and 3.7', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), 6);
+  // SUBTRACT
+  it('should return -2 when subtracting 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
   });
   it('should return 0 when subtracting 0.1 and 0.3', () => {
     assert.strictEqual(calculateNumber('SUBTRACT', 0.1, 0.3), 0);
   });
-  it('should return -4 when subtracting -1 and -3', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), -4);
+  it('should return 2 when subtracting -1 and -3', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
   });
-  it('should return -5 when subtracting -1 and -3.7', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3.7), -5);
+  it('should return 3 when subtracting -1 and -3.7', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3.7), 3);
+  });
+  // DIVIDE
+  it('should return 0.5 when dividing 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.5);
   });
 })
